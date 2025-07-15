4a7b86c9c56491a30c91ffee369c68143c33fba2
ZackLackinJava <brokeskibusiness@gmail.com>
2025-07-15 12:09:42 +0300

    initial EconomyBank.java and plugin.yml basic setup

--- src/main/java/org/brokeski/economyBank/EconomyBank.java
+++ src/main/java/org/brokeski/economyBank/EconomyBank.java
@@ -4,14 +4,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EconomyBank extends JavaPlugin {
 
+    private static EconomyBank instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        instance = this;
+        saveDefaultConfig();
+        getLogger().info("EconomyBank has been enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("EconomyBank has been disabled");
+    }
+
+    public static EconomyBank getInstance(){
+        return instance;
     }
 }
